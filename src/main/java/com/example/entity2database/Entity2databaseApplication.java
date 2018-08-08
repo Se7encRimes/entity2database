ce40c0cd2f4ee3152c079d1a0db3b064b5cb5e68
Se7encRimes <1148872740@qq.com>
2018-08-08 15:27:15 +0800

    Initial commit

---
+++ src/main/java/com/example/entity2database/Entity2databaseApplication.java
@@ -0,0 +1,12 @@
+package com.example.entity2database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Entity2databaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Entity2databaseApplication.class, args);
+    }
+}
